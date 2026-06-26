27880df8a2e177b7ca1c7f7a7a613a4d7a9fd8f3
valdoblair <valdoblair@MacBook-Pro-de-valdoblair.local>
2026-06-26 06:15:15 -0700

    fix(ci): simplify PATH fix for Xcode Cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,21 +2,9 @@
 
 set -e
 
-# Install Homebrew if needed
-if ! command -v brew &> /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install Node via nvm or brew
-export NVM_DIR="$HOME/.nvm"
-if [ -s "$NVM_DIR/nvm.sh" ]; then
-  . "$NVM_DIR/nvm.sh"
-else
-  brew install node
-fi
-
-# Install CocoaPods
-gem install cocoapods
+# Fix PATH for Xcode Cloud
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="/Users/local/Library/mise/shims:$PATH"
 
 cd $CI_PRIMARY_REPOSITORY_PATH
 
